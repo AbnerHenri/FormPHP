65baf8c636255491b767636d1781741942680d35
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-06-25 16:02:32 -0300

    First App

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-    $initial = 'Hello World!';
+
+    class Linguagens {
+        public $name;
+        public $framework;
+        public function __construct($name, $framework){
+            $this->name = $name;
+            $this->framework = $framework;
+        } 
+
+        public function message(){
+            echo "A linguagem é $this->name<br>";
+            echo "O Framework é $this->framework";
+        }
+    }
+
+    $leaguage1 = new Linguagens('JS', 'NextJs');
+    $leaguage2 = new Linguagens('PHP', 'Laravel');
+    $leaguage3 = new Linguagens('Python', 'Django');
+
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +29,9 @@
     <title>Learning PHP</title>
 </head>
 <body>
-    <h1><?php echo $initial ?></h1>
+    <?php 
+        echo $leaguage3->message();
+    ?>
 </body>
 </html>
 
