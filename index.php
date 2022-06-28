20a2e8f897b0f383841b92d61d130ed5679a8284
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-06-27 22:11:23 -0300

    Learning GET_One

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-    $alunos = array(
-        "Aluno 1"=>"Abner",
-        "Aluno 2"=>"José",
-        "Aluno 3"=>"Giovanna",
-        "Aluno 4"=>"Larissa"
-    )
+if(isset($_GET['nome'])){
+    $nome = $_GET['nome'];
+}else{
+    $nome = 'Mundo';
+} 
 ?>
 
 <!DOCTYPE html>
@@ -16,11 +15,12 @@
     <title>Learning PHP</title>
 </head>
 <body>
-    <?php 
-        foreach($alunos as $key => $value){
-            echo "<strong>$key</strong> : $value<br>";
-        }
-    ?>
+    <h1>
+        Olá <?php echo $nome ?>
+    </h1>
+    <a href='index.php?nome=Abner'>Ir para Abner</a><br>
+    <a href='index.php?nome=José'>Ir para José</a><br>
+    <a href='index.php?nome=Larissa'>Ir para Larissa</a><br>
 </body>
 </html>
 
