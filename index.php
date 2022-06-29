9e38bbbe3f0c17cafca953188d441c2ead31a8c0
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-06-29 14:00:54 -0300

    Add Form HTML

--- index.php
+++ index.php
@@ -8,7 +8,31 @@
 </head>
 <body>
     <div class="Container">
-        
+        <div class="Login">
+            <div class="Name Input">
+                <input type="text" name="name" placeholder="Nome" />
+                <span>Erro Nome</span>
+            </div>
+
+            <div class="Email Input">
+                <input type="text" name="email" placeholder="E-mail" />
+                <span>Erro E-mail</span>
+            </div>
+
+            <div class="Password Input">
+                <input type="text" name="password" placeholder="Password" />
+                <span>Erro Password</span>
+            </div>
+
+            <div class="Confirm Input">
+                <input type="text" name="confirm" placeholder="Repita a senha" />
+                <span>Erro Confirm</span>
+            </div>
+        </div>
+
+        <div class="Welcome">
+             
+        </div>
     </div>
 </body>
 </html>
